dc111e7ae5583114f52a10aad14c0af1b729dfd4
parthrugved <parthrugved@gmail.com>
2025-11-27 20:01:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function addTask() {
+    // Selecting elements using DOM
+
+    const addBtn = document.getElementById('add-todo')
+    const input = document.getElementById('input-todo')
+    const task = document.getElementById('task-list')
+
+}
